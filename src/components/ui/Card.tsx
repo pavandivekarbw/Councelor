3652e1dd3d0565741b5a99f2473962d7fdae48ca
pavandivekarbw <pavan.divekar@boardwalktech.com>
2025-08-28 19:54:05 +0530

    bug fixes

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactNode } from "react";
+import type { ReactNode } from "react";
 
 interface CardProps {
     children: ReactNode;
--- src/pages/integration/loading.tsx
+++ src/pages/integration/loading.tsx
@@ -1,4 +1,5 @@
-import Loader from "../components/loader/Loader";
+// Make sure the path is correct; for example, if the Loader component is in 'src/components/loader/Loader.tsx', use:
+import Loader from "../../components/loader/Loader";
 
 export default function Loading() {
     return <Loader />;
